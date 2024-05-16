373364f9fe8fe121dae1e6378557277a9be4cc06
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2024-05-16 12:46:56 +0300

    update: player health management

--- Assets/Survivor-Project/Scripts/Health/PlayerHealth.cs
+++ Assets/Survivor-Project/Scripts/Health/PlayerHealth.cs
@@ -1,11 +1,46 @@
 using UnityEngine;
+using System.Threading.Tasks;
 public class PlayerHealth : Health
 {
-    [SerializeField] private PlayerSO playerSO;
-    private void Start()
+
+    [SerializeField] protected PlayerSO playerSO;
+    [SerializeField] private Material playerDamagedMaterial;
+    private Material playerMat;
+    private MeshRenderer playermeshRenderer;
+
+    private void Awake()
     {
         health = playerSO.health;
         maxHealth = playerSO.maxHealth;
         health = maxHealth;
+        playerMat = GetComponent<MeshRenderer>().material;
+        playermeshRenderer = GetComponent<MeshRenderer>();
+    }
+    private void Start()
+    {
+        OnTakeDamage += HandleTakeDamage;
+        OnDie += HandleDie;
+    }
+    private void OnDestroy()
+    {
+        OnTakeDamage -= HandleTakeDamage;
+        OnDie -= HandleDie;
+    }
+    private void HandleTakeDamage()
+    {
+        Debug.Log("Take Damage");
+        MakeMaterialRed();
+    }
+    private void HandleDie()
+    {
+        Debug.Log("Die");
+        //GameManager.EndGame();
+    }
+    private async void MakeMaterialRed()
+    {
+        playermeshRenderer.material = playerDamagedMaterial;
+        await Task.Delay(100);
+        playermeshRenderer.material = playerMat;
     }
 }
+ 
