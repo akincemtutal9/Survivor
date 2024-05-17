473db913fd356404acec6e8dcf9f104e65d3e0c3
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2024-05-17 12:59:49 +0300

    feat: Skill List

--- Assets/Survivor-Project/Scripts/Player/Damage.cs
+++ Assets/Survivor-Project/Scripts/Player/Damage.cs
@@ -4,36 +4,46 @@ using System.Threading.Tasks;
 
 public class Damage : MonoBehaviour
 {
-    [SerializeField] private Fireball fireballPrefab; // Skill prefabs
-    [SerializeField] private Iceball iceballPrefab; // Skill prefabs
-
+    [SerializeField] private List<Skill> availableSkills; // Skill prefabs list
     private Dictionary<Skill, bool> skillCooldowns = new Dictionary<Skill, bool>();
 
     private void Start()
     {
-        // Initialize the cooldown dictionary
-        skillCooldowns[fireballPrefab] = true;
-        skillCooldowns[iceballPrefab] = true;
+        // Initialize the cooldown dictionary for each skill in the availableSkills list
+        foreach (var skill in availableSkills)
+        {
+            skillCooldowns[skill] = true;
+        }
     }
+
     private void Update()
     {
-        // Example of casting different skills
-        if (skillCooldowns[fireballPrefab])
+        // Example of casting different skills from the availableSkills list
+        foreach (var skill in availableSkills)
         {
-            CastSkill(fireballPrefab);
-        }
-
-        if (skillCooldowns[iceballPrefab])
-        {
-            CastSkill(iceballPrefab);
+            if (skillCooldowns[skill])
+            {
+                CastSkill(skill);
+            }
         }
     }
+
+    public void AddSkill(Skill skillPrefab)
+    {
+        if (!availableSkills.Contains(skillPrefab))
+        {
+            availableSkills.Add(skillPrefab);
+            skillCooldowns[skillPrefab] = true;
+        }
+    }
+
     private void CastSkill<T>(T skillPrefab) where T : Skill
     {
         Instantiate(skillPrefab, transform.position, transform.rotation);
         skillCooldowns[skillPrefab] = false;
         ResetCooldown(skillPrefab);
     }
+
     private async void ResetCooldown<T>(T skill) where T : Skill
     {
         await Task.Delay((int)skill.skillData.skillCooldown * 1000);
